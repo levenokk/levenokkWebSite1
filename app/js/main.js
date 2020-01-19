bf991e9ffc038ecee9be484062ac430903504fde
levenokk <levenokk@yandex.ru>
2020-01-19 20:41:53 +0300

    added full adaptation, preloader and arrow up

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,38 @@ $(function () {
         autoplaySpeed: 5000,
         nextArrow: '<a href="#" class="next-arrow"><i class="fas fa-angle-right"></i></a>',
         prevArrow: '<a href="#" class="prev-arrow"><i class="fas fa-angle-left"></i></a>',
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
+    })
+
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > $(this).height()) {
+            $('.up-arrow').addClass('active');
+        } else {
+            $('.up-arrow').removeClass('active');
+        }
+
+    })
+
+    $('.up-arrow').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    })
+
+    $(window).on('load', function () {
+        document.body.style.overflow = 'hidden';
+        setTimeout(() => {
+            $('.preloader').addClass('done')
+            document.body.style.overflow = '';
+        }, 1000);
     })
 });
 
