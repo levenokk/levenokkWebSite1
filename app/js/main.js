e899fa9cf69ab69ee9b8a62971e53e479dd4932f
levenokk <levenokk@yandex.ru>
2020-01-19 16:08:49 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,16 @@ $(function () {
     $('.menu-btn').on('click', function () {
         $('.header__nav-inner').slideToggle();
     })
+
+    $('.clients-said__slider-wrapper').slick({
+        infinity: true,
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        nextArrow: '<a href="#" class="next-arrow"><i class="fas fa-angle-right"></i></a>',
+        prevArrow: '<a href="#" class="prev-arrow"><i class="fas fa-angle-left"></i></a>',
+    })
 });
 
